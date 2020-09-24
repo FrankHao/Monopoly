c33ab3cebcfec6ad69bdec0f47889092ee78a99d
Rick Cheng <rcsw.devel@gmail.com>
2020-09-23 21:24:41 -0700

    modified EntryUI with tween

--- Assets/Scripts/UI/EntryUI.cs
+++ Assets/Scripts/UI/EntryUI.cs
@@ -1,24 +1,67 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
+using DigitalRuby.Tween;
+
 
 namespace Monopoly.View
 {
-	public class EntryUI : MonoBehaviour {
+    public class EntryUI : MonoBehaviour
+    {
 
-		public delegate void gameStart();
-		public static event gameStart gameStartEvent;
+        public delegate void gameStart();
+        public static event gameStart gameStartEvent;
 
+        [SerializeField] GameObject playButton;
 
-		public void OnClickPlayBtn()
-		{
-			gameObject.SetActive(false);
-			if (gameStartEvent != null)
-			{
-				gameStartEvent();
-			}
-		}
-	}
+        public void OnClickPlayBtn()
+        {
+            playButton.gameObject.SetActive(false);
+            EntryShow();
+
+            if (gameStartEvent != null)
+            {
+                gameStartEvent();
+            }
+        }
+
+        void EntryShow()
+        {
+            TweenRotate();
+            TweenSize();
+        }
+
+        private void TweenRotate()
+        {
+            System.Action<ITween<float>> circleRotate = (t) =>
+            {
+                // start rotation from identity to ensure no stuttering
+                transform.rotation = Quaternion.identity;
+                transform.Rotate(Camera.main.transform.forward, t.CurrentValue);
+            };
+
+            float startAngle = transform.rotation.eulerAngles.z;
+            float endAngle = startAngle + 720.0f + 45f;
+
+            // completion defaults to null if not passed in
+            gameObject.Tween("RotateCircle", startAngle, endAngle, 1.0f, TweenScaleFunctions.CubicEaseInOut, circleRotate);
+        }
+        private void TweenSize()
+        {
+            System.Action<ITween<float>> circleSize = (t) =>
+            {
+                // start rotation from identity to ensure no stuttering
+                transform.rotation = Quaternion.identity;
+                transform.localScale = Vector3.one * t.CurrentValue;
+            };
+
+            float startAngle = 1;
+            float endAngle = 0.75f;
+
+            // completion defaults to null if not passed in
+            gameObject.Tween("ResizeCircle", startAngle, endAngle, 1.0f, TweenScaleFunctions.CubicEaseInOut, circleSize);
+        }
+
+    }
 }
 
 
