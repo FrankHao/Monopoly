6c6760fe45c17de2e4777e0125b979a371ea2785
Rick Cheng <rcsw.devel@gmail.com>
2020-09-24 16:31:29 -0700

    Refactor tween and logo

---
+++ Assets/Scripts/Common/TweenUtil.cs
@@ -0,0 +1,51 @@
+﻿using DigitalRuby.Tween;
+using UnityEngine;
+
+public class TweenUtil : MonoBehaviour
+{
+    public static void TweenRotate(Transform transform, float endAngle, float duration = 1f)
+    {
+        System.Action<ITween<float>> objectRotate = (t) =>
+        {
+            // start rotation from identity to ensure no stuttering
+            transform.rotation = Quaternion.identity;
+            transform.Rotate(Camera.main.transform.forward, t.CurrentValue);
+        };
+
+        float startAngle = transform.rotation.eulerAngles.z;
+
+        // completion defaults to null if not passed in
+        transform.gameObject.Tween("RotateTrans_" + transform.name, startAngle, endAngle, duration, TweenScaleFunctions.CubicEaseInOut, objectRotate);
+    }
+
+    public static void TweenSize(Transform transform, float endSize, float duration = 1f)
+    {
+        System.Action<ITween<float>> objectSize = (t) =>
+        {
+            // start rotation from identity to ensure no stuttering
+            transform.rotation = Quaternion.identity;
+            transform.localScale = Vector3.one * t.CurrentValue;
+        };
+
+        float startSize = 1;
+
+        // completion defaults to null if not passed in
+        transform.gameObject.Tween("ResizeTrans_" + transform.name, startSize, endSize, duration, TweenScaleFunctions.CubicEaseIn, objectSize);
+    }
+
+    public static void TweenAlpha(Transform transform, float endAlpha, float duration = 1f)
+    {
+        SpriteRenderer sprite = transform.GetComponent<SpriteRenderer>();
+        if (sprite == null) return;
+
+        System.Action<ITween<float>> objectAlpha = (t) =>
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, t.CurrentValue);
+        };
+
+        float startAlpha = sprite.color.a;
+
+        // completion defaults to null if not passed in
+        transform.gameObject.Tween("ResizeTrans_" + transform.name, startAlpha, endAlpha, duration, TweenScaleFunctions.CubicEaseOut, objectAlpha);
+    }
+}
--- Assets/Scripts/UI/EntryUI.cs
+++ Assets/Scripts/UI/EntryUI.cs
@@ -26,39 +26,8 @@ namespace Monopoly.View
 
         void EntryShow()
         {
-            TweenRotate();
-            TweenSize();
-        }
-
-        private void TweenRotate()
-        {
-            System.Action<ITween<float>> circleRotate = (t) =>
-            {
-                // start rotation from identity to ensure no stuttering
-                transform.rotation = Quaternion.identity;
-                transform.Rotate(Camera.main.transform.forward, t.CurrentValue);
-            };
-
-            float startAngle = transform.rotation.eulerAngles.z;
-            float endAngle = startAngle + 720.0f + 45f;
-
-            // completion defaults to null if not passed in
-            gameObject.Tween("RotateCircle", startAngle, endAngle, 1.0f, TweenScaleFunctions.CubicEaseInOut, circleRotate);
-        }
-        private void TweenSize()
-        {
-            System.Action<ITween<float>> circleSize = (t) =>
-            {
-                // start rotation from identity to ensure no stuttering
-                transform.rotation = Quaternion.identity;
-                transform.localScale = Vector3.one * t.CurrentValue;
-            };
-
-            float startAngle = 1;
-            float endAngle = 0.75f;
-
-            // completion defaults to null if not passed in
-            gameObject.Tween("ResizeCircle", startAngle, endAngle, 1.0f, TweenScaleFunctions.CubicEaseInOut, circleSize);
+            TweenUtil.TweenRotate(transform, transform.rotation.eulerAngles.z + 720f + 45f);
+            TweenUtil.TweenSize(transform, 0.75f);
         }
 
     }
